a776f18e632b183303f30e29559b2208ced3343c
Bernardo Costa <bfvcc01@gmail.com>
2023-09-02 13:45:44 -0300

    teste

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const btnMobile = document.getElementById("btn-mobile");
+
+function toggleMenu(event) {
+  if (event.type === "touchstart") {
+    event.preventDefault();
+  }
+  const nav = document.querySelector("#nav");
+  nav.classList.toggle("ativo");
+  btnMobile.classList.toggle('ativo')
+}
+
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
